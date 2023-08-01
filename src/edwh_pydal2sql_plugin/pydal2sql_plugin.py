f50c7d51a6bee85a4bebde6ed48c11bdce58ac5d
Robin <robin.vdn@educationwarehouse.nl>
2023-08-01 11:16:57 +0200

    feat: minimal example with pydal2sql-core

--- src/edwh_pydal2sql_plugin/pydal2sql_plugin.py
+++ src/edwh_pydal2sql_plugin/pydal2sql_plugin.py
@@ -1,6 +1,16 @@
 from invoke import task
 
+from pydal2sql_core.cli_support import core_create, core_alter
+
+# todo:
+#  - other settings,
+#  - edwh migration boilerplate around core output
 
 @task()
-def foo(c):
-    print("Hello, world!")
+def create(_, filename: str, magic: bool = False):
+    return core_create(filename, magic=magic)
+
+
+@task
+def alter(_, file_before: str, file_after: str, magic: bool = False):
+    return core_alter(file_before, file_after, magic=magic)
